00858dab632b653ed4cd62ddfb954ec46b1cf95d
TZY668 <120013862+TZY668@users.noreply.github.com>
2023-03-15 10:25:39 +0800

    Add files via upload

---
+++ while an so on/day03/src/day03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class day03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i, j, k;
+        double l;
+        l = sc.nextDouble();
+        i = sc.nextInt();
+        j = sc.nextInt();
+        k = sc.nextInt();
+        for (; i < 10; i++)
+        {
+            System.out.println("和C++区别不大");
+            while(i<10)
+            {
+                System.out.println("^ ^");
+                i++;
+            }
+        }
+    }
+}
