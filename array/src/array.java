64497ee70e7d917042f375da3a3cf8d445d2fc5c
TZY668 <120013862+TZY668@users.noreply.github.com>
2023-03-30 08:25:52 +0800

    Add files via upload

---
+++ array/src/array.java
@@ -0,0 +1,17 @@
+public class array {
+    public static void main(String[] args) {
+        int Test1[]=new int []{11,22,33,44};
+        double Test2 []=new double []{11.2,11.3,11.4};
+        int  Test3[]={11,22,33,44};
+        // 简写，方便
+        String Test4 []={,"玉树临风先生","风流倜傥女士"};
+        System.out.print(Test1[0]);
+        for(int i=0; i<Test1.length;i++)
+        {
+            System.out.println(Test1[i]);
+        }
+        int []arr=new int [3];
+        // 不简写;
+        
+    }
+}
